4a3e971da5b5f4f063c17f0be82bc6469be2afeb
ZijlerArnold <51839866+ZijlerArnold@users.noreply.github.com>
2020-09-06 16:42:44 +0200

    Add files via upload

---
+++ eerste-javascript/index.js
@@ -0,0 +1,31 @@
+console.log('');
+console.log("               Hello Winc Academy");
+console.log('              ======================');
+console.log('');
+let myName = 'Arnold Zijler';
+let ageNumber = 52;
+let ageString = '52';
+console.log(`                 ${myName}, ${ageNumber} years jong`);
+
+let num1 = 4;
+let num2 = 4;
+let num3 = '4';
+let num4 = '4';
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+let stringsTotal = num3 + num4;
+
+console.log('');
+console.log(`${addition}: This is a Number.`);
+console.log(`${subtraction}: This is a Number.`);
+console.log(`${multiplication}: This is a Number.`);
+console.log(`${division}: This is a Number.`);
+console.log(`${modulus}: This is a Number.`);
+console.log(`${stringsTotal}: Een string is geen nummer dus de uitkomst blijt een string.`);
+console.log('');
+console.log(`${typeof ageNumber}: De typeof resultaat van variable met mijn leeftijd in nummers`);
+console.log(`${typeof ageString}: De typeof resultaat van variable met mijn leeftijd in strings`);
